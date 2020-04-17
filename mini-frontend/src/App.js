8869a31648d927286b3662ad860901b3aac679f6
sasina-sara <60457268+sasina-sara@users.noreply.github.com>
2020-04-18 00:40:45 +0700

    login

--- mini-frontend/src/App.js
+++ mini-frontend/src/App.js
@@ -4,14 +4,28 @@ import "./App.css";
 import LoginForm from "components/LoginForm";
 import MainLayout from "layouts/MainLayout";
 import UserContextProvider from "contexts/UserContext";
+import Login from "pages/Login";
+import { Register } from "pages/Register";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "pages/Home";
 
 const App = () => {
   return (
-    <UserContextProvider>
-      <MainLayout>
-        <LoginForm />
-      </MainLayout>
-    </UserContextProvider>
+    // <UserContextProvider>
+    //   <MainLayout>
+    //     <LoginForm />
+    //   </MainLayout>
+    // </UserContextProvider>
+
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/" component={Home} />
+        {/* <Login />
+        <Register /> */}
+      </Switch>
+    </Router>
   );
 };
 
