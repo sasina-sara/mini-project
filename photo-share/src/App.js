c86fa3f399f51a34c6904b023680d4af8ba19aa8
sasina-sara <60457268+sasina-sara@users.noreply.github.com>
2020-04-22 01:06:02 +0700

    create new project and add material-ui

---
+++ photo-share/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello</Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ photo-share/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "styles/theme";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
