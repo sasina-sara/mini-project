4a56ef8ead6217d5c96d272e9a2132a8da6b21c7
sasina-sara <60457268+sasina-sara@users.noreply.github.com>
2020-04-22 00:45:04 +0700

    cleanup to create new project

--- mini-frontend/src/hooks/useSession.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { UserContext } from "contexts/UserContext";
-
-const useSession = () => {
-  const { user } = useContext(UserContext);
-  return user;
-};
-
-export default useSession;
